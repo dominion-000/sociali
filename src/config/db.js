9931fd400669aa753956a6bf658e95c7fa2cb491
Dominionthedeveloper <Dominionthedeveloper@gmail.com>
2026-04-28 14:16:31 +0100

    feat: add mongodb connection configuration

---
+++ src/config/db.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(
+      process.env.MONGO_URI
+    );
+
+    console.log(
+      `Mongo Connected: ${conn.connection.host}`
+    );
+
+  } catch (error) {
+    console.error(
+      "Database connection failed:",
+      error.message
+    );
+
+    process.exit(1);
+  }
+};
+
+export default connectDB;
